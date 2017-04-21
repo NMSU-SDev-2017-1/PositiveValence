3b47b93b1eb09d6c40158758dd73589fd27df771
Melissa Duran <mduran8@nmsu.edu>
2017-04-20 20:53:58 -0600

    Add existing file

---
+++ Main Project/New Unity Project/Assets/Prefabs/FireEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public GameObject FireParticle;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
