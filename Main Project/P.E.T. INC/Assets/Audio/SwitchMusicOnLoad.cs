5342b7c5730faac27cf793aee93ada80d38fa124
Melissa Duran <mduran8@nmsu.edu>
2017-05-10 23:07:22 -0600

    Music added to nonMinigame scenes

---
+++ Main Project/P.E.T. INC/Assets/Audio/SwitchMusicOnLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMusicOnLoad : MonoBehaviour {
+
+	public AudioClip newTrack;
+	private AudioManager theAM;
+
+	// Use this for initialization
+	void Start () {
+		theAM = FindObjectOfType<AudioManager> ();
+		theAM.ChangeBGM (newTrack);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
